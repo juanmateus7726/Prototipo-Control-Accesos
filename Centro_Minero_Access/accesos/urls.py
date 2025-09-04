1d2f6367051a0e9f4447e666f5b1b4d635108aaf
juanmateus7726 <juancamateus566@gmail.com>
2025-09-04 11:50:06 -0500

    segundo commit

---
+++ Centro_Minero_Access/accesos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('historial/', views.historial, name='accesos_historial'),
+    path('detector/', views.detector_view, name='accesos_detector'),
+]
--- Centro_Minero_Access/accesos/views.py
+++ Centro_Minero_Access/accesos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'accesos/index.html')
+
+
+def historial(request):
+    return render(request, 'accesos/historial.html')
+
+def detector_view(request):
+    return render(request, 'accesos/detector.html')
